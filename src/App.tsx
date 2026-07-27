3e798e62248ce9faeb38895d38ca1a77c3b80318
Frederik Kristensen <frede4947@gmail.com>
2026-07-27 13:38:29 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold text-3xl text-gray-50 '>Frederik Kristensen</h1>
+      <h2 className='font-semibold text-lg text-cyan-400'>Web developer</h2>
+      <h3 className='text-gray-50'>
+        <span className='font-bold'>Email:</span> frede4947@gmail.com
+      </h3>
+      <h3 className='text-gray-50'>
+        <span className='font-bold'>Github:</span> https://github.com/FrederikKristensen
+      </h3>
+    </>
+  )
+}
+
+export default App

92bb1e71cb00932b41f796f5af69bde54a24068d
Frederik Kristensen <frede4947@gmail.com>
2026-07-29 12:58:56 +0200

    Added icons and styled headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import './App.css';
 import Sections from './components/Sections';
+import { MdEmail } from "react-icons/md";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function App() {
 
   return (
     <>
-      <h1 className='font-bold text-3xl text-gray-50 '>Frederik Kristensen</h1>
-      <h2 className='font-semibold text-lg text-cyan-400'>Web developer</h2>
-      <h3 className='text-gray-50'>
-        <span className='font-bold'>Email: </span>frede4947@gmail.com
-      </h3>
-      <h3 className='text-gray-50'>
-        <span className='font-bold'>Github: </span>
-        <a className='text-blue-300 underline underline-offset-2 hover:text-sky-500' href='https://github.com/FrederikKristensen'>Github.com/FrederikKristensen</a>
-      </h3>
-      <h3 className='text-gray-50'>
-        <span className='font-bold'>Linkedin: </span>
-        <a className='text-blue-300 underline underline-offset-2 hover:text-sky-500' href='https://www.linkedin.com/in/frederik-kristensen-94a689198/'>Linkedin.com/in/frederik-kristensen</a>
-      </h3>
+      <h1 className='leading-none font-bold text-3xl text-gray-50 '>Frederik Kristensen</h1>
+      <h2 className='leading-none ml-6 font-semibold text-lg text-cyan-400'>Web developer</h2>
+      <div className=' mt-5 ml-5 mb-4 space-y-3'>
+        <h3 className='flex items-center gap-1.5'>
+          <MdEmail className='text-2xl'/>
+          <span className='font-bold'>Email: </span>frede4947@gmail.com
+        </h3>
+        <h3 className='flex items-center gap-1.5'>
+          <FaGithub className='text-2xl'/>
+          <span className='font-bold'>Github:</span>
+          <a className='text-blue-300 underline underline-offset-2 hover:text-sky-500' href='https://github.com/FrederikKristensen'>Github.com/FrederikKristensen</a>
+        </h3>
+        <h3 className='flex items-center gap-1.5'>
+          <FaLinkedin className='text-2xl'/>
+          <span className='font-bold'>Linkedin: </span>
+          <a className='text-blue-300 underline underline-offset-2 hover:text-sky-500' href='https://www.linkedin.com/in/frederik-kristensen-94a689198/'>Linkedin.com/in/frederik-kristensen</a>
+        </h3>
+      </div>
       <Sections />
     </>
   )
